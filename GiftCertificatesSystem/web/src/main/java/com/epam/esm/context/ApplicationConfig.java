f7661e49e82696e6eaeb2c8e0a54eb94d3559846
YuKopilets <yu.kopilets@gmail.com>
2020-10-28 08:57:19 +0300

    Update ApplicatinConfig with web project structure

--- GiftCertificatesSystem/web/src/main/java/com/epam/esm/context/ApplicationConfig.java
+++ GiftCertificatesSystem/web/src/main/java/com/epam/esm/context/ApplicationConfig.java
@@ -1,7 +1,12 @@
 package com.epam.esm.context;
 
+import com.epam.esm.service.context.ServiceConfig;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@ComponentScan("com.epam.esm.controller")
+@Import({PersistenceConfig.class, ServiceConfig.class})
 public class ApplicationConfig {
 }
