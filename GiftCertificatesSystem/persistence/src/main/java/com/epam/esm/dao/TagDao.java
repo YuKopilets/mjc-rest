451c588533b0d3a36d7e8f67ee390eb984bb5441
YuKopilets <yu.kopilets@gmail.com>
2020-11-02 11:06:14 +0300

    Clean excess imports

--- GiftCertificatesSystem/persistence/src/main/java/com/epam/esm/dao/TagDao.java
+++ GiftCertificatesSystem/persistence/src/main/java/com/epam/esm/dao/TagDao.java
@@ -2,8 +2,6 @@ package com.epam.esm.dao;
 
 import com.epam.esm.entity.Tag;
 
-import java.util.List;
-
 public interface TagDao extends CrudDao<Tag> {
     void deleteGiftCertificateTagsByTagId(Long tagId);
 }
--- GiftCertificatesSystem/web/src/main/java/com/epam/esm/controller/TagController.java
+++ GiftCertificatesSystem/web/src/main/java/com/epam/esm/controller/TagController.java
@@ -7,7 +7,13 @@ import com.epam.esm.service.TagService;
 import com.epam.esm.service.exception.InvalidRequestedIdServiceException;
 import com.epam.esm.service.exception.TagNotFoundServiceException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import java.util.List;
 
