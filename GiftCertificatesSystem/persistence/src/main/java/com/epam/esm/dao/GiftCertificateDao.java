343a249af669ef5e2539b341276bf13c45cfab3f
YuKopilets <yu.kopilets@gmail.com>
2020-11-05 12:38:15 +0300

    Add javadocs for public methods of dao and service layers

--- GiftCertificatesSystem/persistence/src/main/java/com/epam/esm/dao/GiftCertificateDao.java
+++ GiftCertificatesSystem/persistence/src/main/java/com/epam/esm/dao/GiftCertificateDao.java
@@ -5,10 +5,38 @@ import com.epam.esm.util.GiftCertificateQuery;
 
 import java.util.List;
 
+/**
+ * The interface Gift certificate dao is expansion of CrudDao for additional
+ * operations with <i>gift_Certificate<i/> table and
+ * <i>gift_Certificate_has_tag<i/> cross table.
+ *
+ * @author Yuriy Kopilets
+ * @version 1.0
+ * @see CrudDao
+ */
 public interface GiftCertificateDao extends CrudDao<GiftCertificate> {
+    /**
+     * Save tags to <i>gift_Certificate_has_tag<i/> table.
+     * Create operation (CRUD).
+     *
+     * @param giftCertificate the gift certificate
+     */
     void saveTags(GiftCertificate giftCertificate);
 
+    /**
+     * Find List of all gift certificates by query params.
+     * Read operation (CRUD).
+     *
+     * @param giftCertificateQuery the gift certificate query
+     * @return the list of all found gift certificates
+     */
     List<GiftCertificate> findAllByQueryParams(GiftCertificateQuery giftCertificateQuery);
 
+    /**
+     * Delete tags by id from <i>gift_Certificate_has_tag<i/> table.
+     * Delete operation (CRUD).
+     *
+     * @param giftCertificateId the gift certificate id
+     */
     void deleteTagsById(Long giftCertificateId);
 }
--- GiftCertificatesSystem/service/src/main/java/com/epam/esm/service/TagService.java
+++ GiftCertificatesSystem/service/src/main/java/com/epam/esm/service/TagService.java
@@ -7,14 +7,57 @@ import com.epam.esm.service.exception.TagNotFoundServiceException;
 
 import java.util.List;
 
+/**
+ * The interface Tag service.
+ *
+ * @author Yuriy Kopilets
+ * @version 1.0
+ */
 public interface TagService {
+    /**
+     * Add new tag.
+     *
+     * @param tag the tag
+     * @return the tag
+     */
     Tag addTag(Tag tag);
 
+    /**
+     * Get single tag by id.
+     *
+     * @param id the tag id
+     * @return the tag by id
+     * @throws TagNotFoundServiceException        in case of {@code tag with
+     * current id not found}
+     * @throws InvalidRequestedIdServiceException in case of {@code id is not
+     * valid to do operation}
+     */
     Tag getTagById(Long id) throws TagNotFoundServiceException, InvalidRequestedIdServiceException;
 
+    /**
+     * Get list of all tags.
+     *
+     * @return the list of all exists tags
+     */
     List<Tag> getAllTags();
 
+    /**
+     * Remove tag.
+     *
+     * @param id the tag id
+     * @throws InvalidRequestedIdServiceException  in case of {@code id is not
+     * valid to do operation}
+     * @throws DeleteByRequestedIdServiceException in case of {@code tag by
+     * current id hasn't been deleted}
+     */
     void removeTag(Long id) throws InvalidRequestedIdServiceException, DeleteByRequestedIdServiceException;
 
+    /**
+     * Remove tag in gift certificates.
+     *
+     * @param tagId the tag id
+     * @throws InvalidRequestedIdServiceException in case of {@code id is not
+     * valid to do operation}
+     */
     void removeGiftCertificateTags(Long tagId) throws InvalidRequestedIdServiceException;
 }
