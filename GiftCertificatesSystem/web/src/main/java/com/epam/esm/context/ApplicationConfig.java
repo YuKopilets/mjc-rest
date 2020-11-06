66f7b2412a449a55dc1b43048f211afd3fa22221
YuKopilets <yu.kopilets@gmail.com>
2020-11-06 16:55:09 +0300

    Update project configuration with spring boot

--- GiftCertificatesSystem/web/src/main/java/com/epam/esm/context/ApplicationConfig.java
+++ GiftCertificatesSystem/web/src/main/java/com/epam/esm/context/ApplicationConfig.java
@@ -1,15 +1,36 @@
 package com.epam.esm.context;
 
 import com.epam.esm.service.context.ServiceConfig;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+import java.util.Arrays;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
 @ComponentScan("com.epam.esm")
 @Import({PersistenceConfig.class, ServiceConfig.class})
-@EnableWebMvc
-public class ApplicationConfig implements WebMvcConfigurer {
+public class ApplicationConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfig.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
 }
+
