4d27677986b96ba7d9c91fb13bd00ee865e5cbb7
YuKopilets <yu.kopilets@gmail.com>
2020-11-11 13:56:53 +0300

    Update configurations property files

--- GiftCertificatesSystem/persistence/src/main/java/com/epam/esm/context/PersistenceConfig.java
+++ GiftCertificatesSystem/persistence/src/main/java/com/epam/esm/context/PersistenceConfig.java
@@ -19,6 +19,8 @@ public class PersistenceConfig {
     private static final String PACKAGE_TO_SCAN = "com.epam.esm.entity";
     private static final String DDL_AUTO_PROPERTY = "hibernate.hbm2ddl.auto";
     private static final String DIALECT_PROPERTY = "hibernate.dialect";
+    private static final String SHOW_SQL_PROPERTY = "hibernate.show_sql";
+    private static final String FORMAT_SQL_PROPERTY = "hibernate.format_sql";
 
     private final Environment environment;
 
@@ -35,6 +37,8 @@ public class PersistenceConfig {
         Properties hibernateProperties = new Properties();
         hibernateProperties.setProperty(DDL_AUTO_PROPERTY, environment.getProperty(DDL_AUTO_PROPERTY));
         hibernateProperties.setProperty(DIALECT_PROPERTY, environment.getProperty(DIALECT_PROPERTY));
+        hibernateProperties.setProperty(SHOW_SQL_PROPERTY, environment.getProperty(SHOW_SQL_PROPERTY));
+        hibernateProperties.setProperty(FORMAT_SQL_PROPERTY, environment.getProperty(FORMAT_SQL_PROPERTY));
         return hibernateProperties;
     }
 }
