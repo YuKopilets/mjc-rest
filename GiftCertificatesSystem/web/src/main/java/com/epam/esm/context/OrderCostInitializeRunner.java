96da0b40868767377fa69ad06aacdd147b1bed8a
YuKopilets <yu.kopilets@gmail.com>
2020-11-13 11:09:08 +0300

    Add javadoc for runner class

--- GiftCertificatesSystem/web/src/main/java/com/epam/esm/context/OrderCostInitializeRunner.java
+++ GiftCertificatesSystem/web/src/main/java/com/epam/esm/context/OrderCostInitializeRunner.java
@@ -5,6 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+/**
+ * The {@code Order cost initialize runner} scans orders in database
+ * and recalculates them cost when it's necessary.
+ *
+ * @author Yuriy Kopilets
+ * @version 1.0
+ * @see CommandLineRunner
+ */
 @Component
 @RequiredArgsConstructor
 public class OrderCostInitializeRunner implements CommandLineRunner {
