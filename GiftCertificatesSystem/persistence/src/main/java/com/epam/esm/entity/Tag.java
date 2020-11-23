cb7dc17e6a9459233e29d7a4a9da2731efdc1dae
YuKopilets <yu.kopilets@gmail.com>
2020-11-23 17:11:01 +0300

    Delete excess imports

--- GiftCertificatesSystem/persistence/src/main/java/com/epam/esm/entity/Tag.java
+++ GiftCertificatesSystem/persistence/src/main/java/com/epam/esm/entity/Tag.java
@@ -9,7 +9,14 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.envers.Audited;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
