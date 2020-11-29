a5b0d86f7f901de59a2f7855bdb9598bf3dd68ca
YuKopilets <yu.kopilets@gmail.com>
2020-11-29 16:12:22 +0300

    Add oauth authentication with google and github

--- GiftCertificatesSystem/web/src/main/java/com/epam/esm/config/WebSecurityConfig.java
+++ GiftCertificatesSystem/web/src/main/java/com/epam/esm/config/WebSecurityConfig.java
@@ -14,6 +14,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -39,6 +46,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     };
 
     private final UserDetailsService userDetailsService;
+    private final OidcUserService oidcUserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -57,17 +65,43 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/", true)
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
                     .permitAll()
+                .and()
+                    .oauth2Login()
+                        .loginPage("/oauth")
+                        .defaultSuccessUrl("/", true)
+                        .authorizationEndpoint()
+                        .baseUri("/oauth2/authorize-client")
+                        .authorizationRequestRepository(authorizationRequestRepository())
+                .and()
+                    .tokenEndpoint()
+                        .accessTokenResponseClient(accessTokenResponseClient())
+                .and()
+                    .userInfoEndpoint()
+                        .oidcUserService(oidcUserService)
+                .and()
+                    .permitAll(false)
                 .and()
                     .logout()
                     .permitAll()
                 .and()
                     .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+
                     .addFilterBefore(new AuthorizationFilter(userDetailsService),
                             UsernamePasswordAuthenticationFilter.class)
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
+        return new HttpSessionOAuth2AuthorizationRequestRepository();
+    }
+
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+        return new DefaultAuthorizationCodeTokenResponseClient();
     }
 
     @Bean
